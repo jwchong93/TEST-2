cdf22e9c7f01dd961a89a50dca9759df62706a11
jwchong93 <jwchong93@gmail.com>
2014-03-08 22:29:59 +0800

    Modified the test for reverse element and added test for wasEstablishedBefore.

--- test/test_institution.c
+++ test/test_institution.c
@@ -2,10 +2,11 @@
 #include "institution.h"
 #include "mock_LinkedList.h"
 #include "mock_Stack.h"
-#include <malloc.h>
+#include "CException.h"
 
 void setUp(){}
 void tearDown(){}
+
 
 void test_Insitution_reverse_will_2_elements_of_the_input_Linkedlist()
 {
@@ -20,8 +21,8 @@ void test_Insitution_reverse_will_2_elements_of_the_input_Linkedlist()
 	Stack testStack;
 	
 	//Since the input list is empty, this function will return a 0.
-	determinder=Institution_reverse(&testList1,&testStack,&testList1);
-	TEST_ASSERT_EQUAL(0,determinder);
+	//determinder=Institution_reverse(&testList1,&testStack,&testList1);
+	//TEST_ASSERT_EQUAL(0,determinder);
 	
 	//Function mocking
 	List_removeHead_ExpectAndReturn(&testList1,&element1);
@@ -40,163 +41,136 @@ void test_Insitution_reverse_will_2_elements_of_the_input_Linkedlist()
 	determinder=Institution_reverse(&testList1,&testStack,&testList2);
 	TEST_ASSERT_EQUAL(1,determinder);
 }
-/*	
+
+
+void test_Insitution_reverse_will_3_elements_of_the_input_Linkedlist()
+{
+
+	int determinder;
+	LinkedList  testList1 ;
+	LinkedList  testList2 ;
 	
-	//Add element 1 ,2&3 into the list and try to reverse it.
-	testList1->head = element1;
-	element1->next = element2;
-	element2->next=element3;
-	element3->next=NULL;
+	Institution element1;
+	Institution element2;
+	Institution element3;
+	
+	Stack testStack;
 	
 
 	//Function mocking
-	List_removeHead_ExpectAndReturn(testList1,element1);
-	Stack_push_Expect(testStack,element1);
-	List_removeHead_ExpectAndReturn(testList1,element2);
-	Stack_push_Expect(testStack,element2);
-	List_removeHead_ExpectAndReturn(testList1,element3);
-	Stack_push_Expect(testStack,element3);
-	List_removeHead_ExpectAndReturn(testList1,NULL);
+	List_removeHead_ExpectAndReturn(&testList1,&element1);
+	Stack_push_Expect(&testStack,&element1);
+	List_removeHead_ExpectAndReturn(&testList1,&element2);
+	Stack_push_Expect(&testStack,&element2);
+	List_removeHead_ExpectAndReturn(&testList1,&element3);
+	Stack_push_Expect(&testStack,&element3);
+	List_removeHead_ExpectAndReturn(&testList1,NULL);
 
-	testStack->topOfStack=element3;
-	element3->next=element2;
-	element2->next=element1;
-	element1->next=NULL;
 	
-	Stack_pop_ExpectAndReturn(testStack,element3);
-	List_addTail_Expect(testList2,element3);
-	Stack_pop_ExpectAndReturn(testStack,element2);
-	List_addTail_Expect(testList2,element2);
-	Stack_pop_ExpectAndReturn(testStack,element1);
-	List_addTail_Expect(testList2,element1);
-	Stack_pop_ExpectAndReturn(testStack,NULL);
+	Stack_pop_ExpectAndReturn(&testStack,&element3);
+	List_addTail_Expect(&testList2,&element3);
+	Stack_pop_ExpectAndReturn(&testStack,&element2);
+	List_addTail_Expect(&testList2,&element2);
+	Stack_pop_ExpectAndReturn(&testStack,&element1);
+	List_addTail_Expect(&testList2,&element1);
+	Stack_pop_ExpectAndReturn(&testStack,NULL);
 	
-	determinder=Institution_reverse(testList1,testStack,testList2);
+	determinder=Institution_reverse(&testList1,&testStack,&testList2);
 	TEST_ASSERT_EQUAL(1,determinder);
 	
-	
-	//Add element 1,2,3&4 into the list and try to reverse it.
-	testList1->head = element1;
-	element1->next = element2;
-	element2->next=element3;
-	element3->next=element4;
-	element4->next=NULL;
-	
-
-	//Function mocking
-	List_removeHead_ExpectAndReturn(testList1,element1);
-	Stack_push_Expect(testStack,element1);
-	List_removeHead_ExpectAndReturn(testList1,element2);
-	Stack_push_Expect(testStack,element2);
-	List_removeHead_ExpectAndReturn(testList1,element3);
-	Stack_push_Expect(testStack,element3);
-	List_removeHead_ExpectAndReturn(testList1,element4);
-	Stack_push_Expect(testStack,element4);
-	List_removeHead_ExpectAndReturn(testList1,NULL);
-
-	testStack->topOfStack=element3;
-	element3->next=element2;
-	element2->next=element1;
-	element1->next=NULL;
-	
-	
-	Stack_pop_ExpectAndReturn(testStack,element4);
-	List_addTail_Expect(testList2,element4);
-	Stack_pop_ExpectAndReturn(testStack,element3);
-	List_addTail_Expect(testList2,element3);
-	Stack_pop_ExpectAndReturn(testStack,element2);
-	List_addTail_Expect(testList2,element2);
-	Stack_pop_ExpectAndReturn(testStack,element1);
-	List_addTail_Expect(testList2,element1);
-	Stack_pop_ExpectAndReturn(testStack,NULL);
-	
-	determinder=Institution_reverse(testList1,testStack,testList2);
-	TEST_ASSERT_EQUAL(1,determinder);
-	free(element1);
-	free(element2);
-	free(element3);
-	free(element4);
-	free(testList1);
-	free(testList2);
 }
-*/
+void test_Insitution_reverse_will_4_elements_of_the_input_Linkedlist()
+{
+
+	int determinder;
+	LinkedList  testList1 ;
+	LinkedList  testList2 ;
+	
+	Institution element1;
+	Institution element2;
+	Institution element3;
+	Institution element4;
+	
+	Stack testStack;
+	
+
+
+	//Function mocking
+	List_removeHead_ExpectAndReturn(&testList1,&element1);
+	Stack_push_Expect(&testStack,&element1);
+	List_removeHead_ExpectAndReturn(&testList1,&element2);
+	Stack_push_Expect(&testStack,&element2);
+	List_removeHead_ExpectAndReturn(&testList1,&element3);
+	Stack_push_Expect(&testStack,&element3);
+	List_removeHead_ExpectAndReturn(&testList1,&element4);
+	Stack_push_Expect(&testStack,&element4);
+	List_removeHead_ExpectAndReturn(&testList1,NULL);
+	
+	
+	Stack_pop_ExpectAndReturn(&testStack,&element4);
+	List_addTail_Expect(&testList2,&element4);
+	Stack_pop_ExpectAndReturn(&testStack,&element3);
+	List_addTail_Expect(&testList2,&element3);
+	Stack_pop_ExpectAndReturn(&testStack,&element2);
+	List_addTail_Expect(&testList2,&element2);
+	Stack_pop_ExpectAndReturn(&testStack,&element1);
+	List_addTail_Expect(&testList2,&element1);
+	Stack_pop_ExpectAndReturn(&testStack,NULL);
+	
+	determinder=Institution_reverse(&testList1,&testStack,&testList2);
+	TEST_ASSERT_EQUAL(1,determinder);
+}
+
+
 void test_Institution_select_will_filter_out_specific_type()
 {
-	LinkedList * testList1 = malloc(sizeof(LinkedList));
-	LinkedList * testList2 = malloc(sizeof(LinkedList));
-	testList1->head=testList1->tail= NULL;
+	LinkedList testList1,testList2;
 	
-	Institution *element1 = malloc(sizeof(Institution));
-	Institution *element2 = malloc(sizeof(Institution));
-	Institution *element3 = malloc(sizeof(Institution));
-	Institution *element4 = malloc(sizeof(Institution));
+	Institution element1,element2,element3,element4;
 	
-	element1->type=UniversityCollege;
-	element2->type=College;
-	element3->type=University;
-	element4->type=College;
+	element1.type=UniversityCollege;
+	element2.type=College;
+	element3.type=University;
+	element4.type=College;
 	
-	//Try work with two elements.
-	testList1->head = element1;
-	element1->next = element2;
-	element2->next =NULL;
 	
 	InstitutionType type =College;
 	
-	List_removeHead_ExpectAndReturn(testList1,element1);
-	List_removeHead_ExpectAndReturn(testList1,element2);
-	List_addTail_Expect(testList2,element2);
-	List_removeHead_ExpectAndReturn(testList1,NULL);
+	List_removeHead_ExpectAndReturn(&testList1,&element1);
+	List_removeHead_ExpectAndReturn(&testList1,&element2);
+	List_addTail_Expect(&testList2,&element2);
+	List_removeHead_ExpectAndReturn(&testList1,NULL);
 
 	
-	Institution_select(testList1,testList2,&type,isUniversityCollege);
+	Institution_select(&testList1,&testList2,&type,isUniversityCollege);
 	
 	
 	//Increase the element to three.
 	
-	testList1->head = element1;
-	element1->next = element2;
-	element2->next = element3;
-	element3->next = NULL;
 	
-	
-	List_removeHead_ExpectAndReturn(testList1,element1);
-	List_removeHead_ExpectAndReturn(testList1,element2);
-	List_addTail_Expect(testList2,element2);
-	List_removeHead_ExpectAndReturn(testList1,element3);
-	List_removeHead_ExpectAndReturn(testList1,NULL);
+	List_removeHead_ExpectAndReturn(&testList1,&element1);
+	List_removeHead_ExpectAndReturn(&testList1,&element2);
+	List_addTail_Expect(&testList2,&element2);
+	List_removeHead_ExpectAndReturn(&testList1,&element3);
+	List_removeHead_ExpectAndReturn(&testList1,NULL);
 
 	
-	Institution_select(testList1,testList2,&type,isUniversityCollege);
+	Institution_select(&testList1,&testList2,&type,isUniversityCollege);
 	
 	
 	//Increase to four.
 	
-		//Increase the element to three.
 	
-	testList1->head = element1;
-	element1->next = element2;
-	element2->next = element3;
-	element3->next = element4;
-	element4->next=NULL;
-	
-	
-	List_removeHead_ExpectAndReturn(testList1,element1);
-	List_removeHead_ExpectAndReturn(testList1,element2);
-	List_addTail_Expect(testList2,element2);
-	List_removeHead_ExpectAndReturn(testList1,element3);
-	List_removeHead_ExpectAndReturn(testList1,element4);
-	List_addTail_Expect(testList2,element4);
-	List_removeHead_ExpectAndReturn(testList1,NULL);
+	List_removeHead_ExpectAndReturn(&testList1,&element1);
+	List_removeHead_ExpectAndReturn(&testList1,&element2);
+	List_addTail_Expect(&testList2,&element2);
+	List_removeHead_ExpectAndReturn(&testList1,&element3);
+	List_removeHead_ExpectAndReturn(&testList1,&element4);
+	List_addTail_Expect(&testList2,&element4);
+	List_removeHead_ExpectAndReturn(&testList1,NULL);
 
 	
-	Institution_select(testList1,testList2,&type,isUniversityCollege);
-	free(element1);
-	free(element2);
-	free(element3);
-	free(element4);
-	free(testList1);
-	free(testList2);
+	Institution_select(&testList1,&testList2,&type,isUniversityCollege);
 }
 
 void test_Institution_select_the_institution_that_establish_before_1980()
@@ -298,8 +272,8 @@ void test_wasEstablishedBefore_will_throw_an_eror_if_in_input_year_invalid()
 
 	LinkedList testList1,testList2;
 	int year = 1980;
-	Institution elems[]={{.yearEstablished = 2015},
-						{.yearEstablished = 2020},
+	Institution elems[]={{.yearEstablished = 1890},
+						{.yearEstablished = 1955},
 						{.yearEstablished = 3000}};
 	testList1.head=&elems[0];
 	elems[0].next =&elems[1];
@@ -310,27 +284,31 @@ void test_wasEstablishedBefore_will_throw_an_eror_if_in_input_year_invalid()
 	List_removeHead_ExpectAndReturn(&testList1,&elems[0]);
 	List_removeHead_ExpectAndReturn(&testList1,&elems[1]);
 	List_removeHead_ExpectAndReturn(&testList1,&elems[2]);
-	List_removeHead_ExpectAndReturn(&testList1,NULL);
-
+	
+	Try
+	{
 	Institution_select(&testList1,&testList2,&year,wasEstablishedBefore);
-
+	}
+	Catch(Exception)
+	{
+		TEST_ASSERT_EQUAL(INVALID_YEAR,Exception);
+	}
 }
 
 
 void test_isUniversityCollege_will_return_according_to_the_input_parameters()
 {
-	Institution *element1 = malloc(sizeof(Institution));
+	Institution element1;
 	InstitutionType tempType = University;
-	element1->type=College;
+	element1.type=College;
 	int status;
-	status = isUniversityCollege(element1,&tempType);
+	status = isUniversityCollege(&element1,&tempType);
 	TEST_ASSERT_EQUAL(0,status);
 	
 	
 	tempType = College;
-	status = isUniversityCollege(element1,&tempType);
+	status = isUniversityCollege(&element1,&tempType);
 	TEST_ASSERT_EQUAL(1,status);
-	free(element1);
 	
 	
 }
@@ -349,4 +327,22 @@ void test_wasEstablishedBefore_will_return_according_to_the_input_parameters()
 	status = wasEstablishedBefore(&element1,&referenceYear);
 	TEST_ASSERT_EQUAL(1,status);
 
+}
+
+
+void test_wasEstablishedBefore_will_throw_an_error_for_invalid_year()
+{
+	Error exception;
+	Institution element1={.yearEstablished =2020};
+	int referenceYear = 1980; 
+	int status;
+		
+	Try
+	{
+		status = wasEstablishedBefore(&element1,&referenceYear);
+	}
+	Catch(exception)
+	{
+		TEST_ASSERT_EQUAL(INVALID_YEAR,exception);
+	}
 }
